fa6e9ab3d85d39f49bf0a34c6b43bbb65c48c571
bigzod <Christetienne15@yahoo.fr>
2014-08-23 23:52:24 +0630

    transport 1

---
+++ messenger.py
@@ -0,0 +1,11 @@
+p=[]
+f=open("number1.txt","r")
+p.append(f)
+def messenger(p):
+    for n in p:
+        if n ==152:
+            print(n)
+        else:
+            pass
+print (messenger(p))
+        
